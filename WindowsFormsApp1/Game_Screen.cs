c6b8c3c928df5e831d99a727e05d029be6f983e4
nya <elim9010@gmail.com>
2022-05-23 17:05:27 +0900

    소라 병합

--- WindowsFormsApp1/Game_Screen.cs
+++ WindowsFormsApp1/Game_Screen.cs
@@ -13,6 +13,7 @@ namespace WindowsFormsApp1
     public partial class Game_Screen : Form
     {
         Market market;
+        List<Card> cardList;
         Deck deck;
         Game game;
         GameTable gameTable;
@@ -27,26 +28,62 @@ namespace WindowsFormsApp1
         private void Form1_Load(object sender, EventArgs e)
         {
             market = new Market();
+            cardList = market.getMarketList();
+            //마켓에 이미지추가하기(Market.cs에서 하려고 하니 이 cs의 것이라 설정이 어려움)
+            pictureBox1.BackColor = Color.AliceBlue;
+            pictureBox2.BackColor = Color.AliceBlue;
+            pictureBox3.BackColor = Color.AliceBlue;
+            pictureBox4.BackColor = Color.AliceBlue;
+            pictureBox5.BackColor = Color.AliceBlue;
+            pictureBox6.BackColor = Color.AliceBlue;
+            pictureBox7.BackColor = Color.AliceBlue;
+            pictureBox8.BackColor = Color.AliceBlue;
+            pictureBox9.BackColor = Color.AliceBlue;
+            pictureBox10.BackColor = Color.AliceBlue;
+
             deck = new Deck();
             game = new Game();
             gameTable = new GameTable();
             trash = new Trash();
             player = new Player(deck,gameTable,market,"pamatto");
 
-
-
-            pictureBox1.BackColor = Color.AliceBlue;
-            pictureBox2.BackColor = Color.GreenYellow;
-
+            listBox1.Items.Clear();
+            listBox1.Items.Add("player1");
+            listBox1.Items.Add("player2");
+            listBox1.Items.Add("player3");
+            listBox1.Items.Add("player4");
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
-            DB_ACCESS db_test = new DB_ACCESS();
-            db_test.SendLog(player,"sending LogTest");
+            int amount = cardList[0].amount;
+            market.SellCard(cardList[0]);
+            amount1.Text = cardList[0].amount.ToString();
+
+            MessageBox.Show(cardList[0].Name + " 카드 " + amount + "개 중 1개를 구입하여 " +
+                cardList[0].amount + "장 남았습니다.");
         }
 
         private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            int amount = cardList[1].amount;
+            market.SellCard(cardList[1]);
+            amount2.Text = cardList[1].amount.ToString();
+
+            MessageBox.Show(cardList[1].Name + " 카드 " + amount + "개 중 1개를 구입하여 " +
+                cardList[1].amount + "장 남았습니다.");
+
+
+            
+        }
+
+        private void pictureBox12_Click(object sender, EventArgs e)
+        {
+            DB_ACCESS db_test = new DB_ACCESS();
+            db_test.SendLog(player, "sending LogTest");
+        }
+
+        private void pictureBox11_Click(object sender, EventArgs e)
         {
             DB_ACCESS db_test = new DB_ACCESS();
             db_test.RecieveLog(listLog);
