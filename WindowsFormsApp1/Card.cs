f0cf8fa9a99ec3857064be7000b0041c66095a3a
소라 오 <osora33@gmail.com>
2022-05-23 15:53:15 +0900

    마켓 생성자 추가 제이슨 파일 읽어들여 마켓에 카드객체 10개 세팅하는 메소드 작성

--- WindowsFormsApp1/Card.cs
+++ WindowsFormsApp1/Card.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace WindowsFormsApp1
 {
     public class Card
     {
-        public json cardInfo = new json();
-        private int price = 0;
-        private int amount = 0;
-        private string Name = "";
+        public int price = 0;
+        public int amount = 0;
+        public string Name = "";
 
         public void setName(string Name)
         {
@@ -51,31 +52,156 @@ namespace WindowsFormsApp1
         //초기값 설정 코드 추가
         public int add_Draw = 0;
         public int goto_Grave = 0;
-        public int goto_Trash { get; set; }
-        public int add_Action { get; set; }
-        public int add_Buy { get; set; }
-        public int add_Money { get; set; }
-        public int gain_to_Deck { get; set; }
-        public int gain_to_Hand { get; set; }
+        public int goto_Trash = 0;
+        public int add_Action = 0;
+        public int add_Buy = 0;
+        public int add_Money = 0;
+        public int gain_to_Deck = 0;
+        public int gain_to_Hand = 0;
         public bool attack = false;
         public bool protect = false;
 
-        public ActionCard(string CardName)
+        public void set_add_Draw(string val)
         {
-            if (CardName.Equals("witch"))
-            {
-                add_Draw = 2;
-                attack = true;
-            }
-            else if (CardName.Equals("market"))
-            {
-
-            }
+            this.add_Draw = Convert.ToInt32(val);
+        }
+        public int get_add_Draw()
+        {
+            return add_Draw;
         }
 
-        public void setIsAttack(bool attack)
+        public void set_goto_Grave(string val)
         {
-            this.attack = attack;
+            this.goto_Grave = Convert.ToInt32(val);
+        }
+        public int get_goto_Grave()
+        {
+            return goto_Grave;
+        }
+
+        public void set_goto_Trash(string val)
+        {
+            this.goto_Trash = Convert.ToInt32(val);
+        }
+        public int get_goto_Trash()
+        {
+            return goto_Trash;
+        }
+
+        public void set_add_Action(string val)
+        {
+            this.add_Action = Convert.ToInt32(val);
+        }
+        public int get_add_Action()
+        {
+            return add_Action;
+        }
+
+        public void set_add_Buy(string val)
+        {
+            this.add_Buy = Convert.ToInt32(val);
+        }
+        public int get_add_Buy()
+        {
+            return add_Buy;
+        }
+
+        public void set_add_Money(string val)
+        {
+            this.add_Money = Convert.ToInt32(val);
+        }
+        public int get_add_Money()
+        {
+            return add_Money;
+        }
+
+        public void set_gain_to_Deck(string val)
+        {
+            this.gain_to_Deck = Convert.ToInt32(val);
+        }
+        public int get_gain_to_Deck()
+        {
+            return gain_to_Deck;
+        }
+
+        public void set_gain_to_Hand(string val)
+        {
+            this.gain_to_Hand = Convert.ToInt32(val);
+        }
+        public int get_gain_to_Hand()
+        {
+            return gain_to_Hand;
+        }
+
+        public void set_attack(string val)
+        {
+            if (val == "true")
+            {
+                this.attack = true;
+            }
+            else{
+                this.attack = false;
+            }
+        }
+        public bool get_attack()
+        {
+            return attack;
+        }
+
+        public void set_protect(string val)
+        {
+            if (val == "true")
+            {
+                this.protect = true;
+            }
+            else
+            {
+                this.protect = false;
+            }
+        }
+        public bool get_protect()
+        {
+            return protect;
+        }
+
+        //생성자
+        public ActionCard(string key, JToken jtoken)
+        {
+            Card card = new Card();
+            //int 타입 멤버변수 배열
+            string[] ops = { "price", "amount", "add_Draw", "goto_Grave", "goto_Trash",
+                        "add_Action", "add_Buy", "add_Money", "gain_to_Deck",
+                        "gain_to_Hand" };
+            setName(key);
+
+            int i = 0;
+            foreach(JToken j in jtoken)
+            { 
+                foreach(JToken j2 in j)
+                {
+                    //효과이름(key값) 얻기위해 JProperty로 형변환
+                    JProperty jp = j2.ToObject<JProperty>();
+                    //효과이름 얻기
+                    string subKey = jp.Name;
+                    setAmount(10);
+
+                    //int 타입 멤버변수일 경우
+                    if(Array.IndexOf(ops, subKey) > -1){
+                        //int값이 들어가야하는 곳에서 n이 들어갈 때 -> 임시로 0으로 해놓기
+                        if (jp.Value.ToString().Equals("n"))
+                        {
+                            jp.Value = 0;
+                        }
+                        //subKey라는 스트링 변수의 값을 변수명으로 가진 변수에 값 세팅
+                        this.GetType().GetField(subKey).SetValue(this, Convert.ToInt32(jp.Value));
+                    }
+                    else
+                    {
+                        //subKey라는 스트링 변수의 값을 변수명으로 가진 변수에 값 세팅
+                        this.GetType().GetField(subKey).SetValue(this, Convert.ToBoolean(jp.Value));
+                    }
+                }
+            }
         }
     }
 
@@ -88,13 +214,4 @@ namespace WindowsFormsApp1
     {
         private int score;
     }
-
-    /*public class witch : ActionCard
-    {
-        public witch()
-        {
-            setPrice(5);
-            setIsAttack(true);
-        }
-    }*/
 }
--- WindowsFormsApp1/Market.cs
+++ WindowsFormsApp1/Market.cs
@@ -3,16 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace WindowsFormsApp1
 {
     class Market : GameTable
     {
-        public List<Card> MarketPile;
+        //마켓에 있는 카드 객체들을 저장할 리스트
+        public List<Card> MarketPile = new List<Card>();
+        public json cardInfo = new json();
+
         public Market()
         {
+            //json 파일 읽어 카드정보들 저장할 변수
+            JObject card = cardInfo.getJson();
+            JToken jtoken = card.First;
 
+            //foreach 조건문 안에서 KeyValuePair 사용하면 오류 없는데 이상하게 안이나 밖에서 쓰면 오류남..
+            //jtoken.Next도 bool타입이 아니니 일단 이렇게 작성하는걸로...
+            foreach (KeyValuePair<string, JToken> pair in card)
+            {
+                //key값을 얻기위해 JProperty형식으로 형변환
+                JProperty jp = jtoken.ToObject<JProperty>();
+                //key값 얻기(카드이름)
+                string key = jp.Name;
+                //액션카드객체 생성
+                ActionCard actionCard = new ActionCard(key, jtoken);
+                //마켓 리스트에 넣기
+                MarketPile.Add(actionCard);
+
+                //다음 카드로
+                jtoken = jtoken.Next;
+            }
         }
+
+        public List<Card> getMarketList()
+        {
+            return MarketPile;
+        }
+
         public void SellCard(Card card)
         {
             card.minusAmount(1);
