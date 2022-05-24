6e00fe80f56832cb63b168241eda1d610680a98f
소라 오 <osora33@gmail.com>
2022-05-24 15:57:04 +0900

    재물, 점수 카드 이미지 추가, 댁 초기화 오류수정

--- Application/WindowsFormsApp1/Game_Screen.Designer.cs
+++ Application/WindowsFormsApp1/Game_Screen.Designer.cs
@@ -52,6 +52,16 @@
             this.pictureBox23 = new System.Windows.Forms.PictureBox();
             this.pictureBox24 = new System.Windows.Forms.PictureBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.amount10 = new System.Windows.Forms.Label();
+            this.amount9 = new System.Windows.Forms.Label();
+            this.amount8 = new System.Windows.Forms.Label();
+            this.amount7 = new System.Windows.Forms.Label();
+            this.amount6 = new System.Windows.Forms.Label();
+            this.amount5 = new System.Windows.Forms.Label();
+            this.amount4 = new System.Windows.Forms.Label();
+            this.amount3 = new System.Windows.Forms.Label();
+            this.amount2 = new System.Windows.Forms.Label();
+            this.amount1 = new System.Windows.Forms.Label();
             this.pictureBox10 = new System.Windows.Forms.PictureBox();
             this.pictureBox5 = new System.Windows.Forms.PictureBox();
             this.pictureBox9 = new System.Windows.Forms.PictureBox();
@@ -76,16 +86,13 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
-            this.amount1 = new System.Windows.Forms.Label();
-            this.amount2 = new System.Windows.Forms.Label();
-            this.amount3 = new System.Windows.Forms.Label();
-            this.amount4 = new System.Windows.Forms.Label();
-            this.amount5 = new System.Windows.Forms.Label();
-            this.amount6 = new System.Windows.Forms.Label();
-            this.amount7 = new System.Windows.Forms.Label();
-            this.amount8 = new System.Windows.Forms.Label();
-            this.amount9 = new System.Windows.Forms.Label();
-            this.amount10 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
             this.groupBox3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox18)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox19)).BeginInit();
@@ -232,6 +239,13 @@
             // 
             // groupBox7
             // 
+            this.groupBox7.Controls.Add(this.label11);
+            this.groupBox7.Controls.Add(this.label10);
+            this.groupBox7.Controls.Add(this.label8);
+            this.groupBox7.Controls.Add(this.label7);
+            this.groupBox7.Controls.Add(this.label6);
+            this.groupBox7.Controls.Add(this.label5);
+            this.groupBox7.Controls.Add(this.label4);
             this.groupBox7.Controls.Add(this.pictureBox17);
             this.groupBox7.Controls.Add(this.pictureBox15);
             this.groupBox7.Controls.Add(this.pictureBox16);
@@ -252,6 +266,7 @@
             this.pictureBox17.Location = new System.Drawing.Point(16, 232);
             this.pictureBox17.Name = "pictureBox17";
             this.pictureBox17.Size = new System.Drawing.Size(34, 72);
+            this.pictureBox17.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox17.TabIndex = 7;
             this.pictureBox17.TabStop = false;
             // 
@@ -261,6 +276,7 @@
             this.pictureBox15.Location = new System.Drawing.Point(122, 139);
             this.pictureBox15.Name = "pictureBox15";
             this.pictureBox15.Size = new System.Drawing.Size(34, 72);
+            this.pictureBox15.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox15.TabIndex = 6;
             this.pictureBox15.TabStop = false;
             // 
@@ -270,6 +286,7 @@
             this.pictureBox16.Location = new System.Drawing.Point(122, 38);
             this.pictureBox16.Name = "pictureBox16";
             this.pictureBox16.Size = new System.Drawing.Size(34, 72);
+            this.pictureBox16.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox16.TabIndex = 5;
             this.pictureBox16.TabStop = false;
             // 
@@ -279,6 +296,7 @@
             this.pictureBox13.Location = new System.Drawing.Point(68, 139);
             this.pictureBox13.Name = "pictureBox13";
             this.pictureBox13.Size = new System.Drawing.Size(34, 72);
+            this.pictureBox13.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox13.TabIndex = 3;
             this.pictureBox13.TabStop = false;
             // 
@@ -288,6 +306,7 @@
             this.pictureBox14.Location = new System.Drawing.Point(15, 139);
             this.pictureBox14.Name = "pictureBox14";
             this.pictureBox14.Size = new System.Drawing.Size(34, 72);
+            this.pictureBox14.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox14.TabIndex = 4;
             this.pictureBox14.TabStop = false;
             // 
@@ -297,6 +316,7 @@
             this.pictureBox11.Location = new System.Drawing.Point(68, 38);
             this.pictureBox11.Name = "pictureBox11";
             this.pictureBox11.Size = new System.Drawing.Size(34, 72);
+            this.pictureBox11.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox11.TabIndex = 1;
             this.pictureBox11.TabStop = false;
             // 
@@ -306,6 +326,7 @@
             this.pictureBox12.Location = new System.Drawing.Point(15, 38);
             this.pictureBox12.Name = "pictureBox12";
             this.pictureBox12.Size = new System.Drawing.Size(34, 72);
+            this.pictureBox12.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox12.TabIndex = 2;
             this.pictureBox12.TabStop = false;
             // 
@@ -366,6 +387,106 @@
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "시장";
+            // 
+            // amount10
+            // 
+            this.amount10.AutoSize = true;
+            this.amount10.ForeColor = System.Drawing.Color.Red;
+            this.amount10.Location = new System.Drawing.Point(434, 155);
+            this.amount10.Name = "amount10";
+            this.amount10.Size = new System.Drawing.Size(16, 14);
+            this.amount10.TabIndex = 10;
+            this.amount10.Text = "10";
+            // 
+            // amount9
+            // 
+            this.amount9.AutoSize = true;
+            this.amount9.ForeColor = System.Drawing.Color.Red;
+            this.amount9.Location = new System.Drawing.Point(330, 155);
+            this.amount9.Name = "amount9";
+            this.amount9.Size = new System.Drawing.Size(16, 14);
+            this.amount9.TabIndex = 9;
+            this.amount9.Text = "10";
+            // 
+            // amount8
+            // 
+            this.amount8.AutoSize = true;
+            this.amount8.ForeColor = System.Drawing.Color.Red;
+            this.amount8.Location = new System.Drawing.Point(226, 155);
+            this.amount8.Name = "amount8";
+            this.amount8.Size = new System.Drawing.Size(16, 14);
+            this.amount8.TabIndex = 8;
+            this.amount8.Text = "10";
+            // 
+            // amount7
+            // 
+            this.amount7.AutoSize = true;
+            this.amount7.ForeColor = System.Drawing.Color.Red;
+            this.amount7.Location = new System.Drawing.Point(117, 155);
+            this.amount7.Name = "amount7";
+            this.amount7.Size = new System.Drawing.Size(16, 14);
+            this.amount7.TabIndex = 7;
+            this.amount7.Text = "10";
+            // 
+            // amount6
+            // 
+            this.amount6.AutoSize = true;
+            this.amount6.ForeColor = System.Drawing.Color.Red;
+            this.amount6.Location = new System.Drawing.Point(20, 155);
+            this.amount6.Name = "amount6";
+            this.amount6.Size = new System.Drawing.Size(16, 14);
+            this.amount6.TabIndex = 6;
+            this.amount6.Text = "10";
+            // 
+            // amount5
+            // 
+            this.amount5.AutoSize = true;
+            this.amount5.ForeColor = System.Drawing.Color.Red;
+            this.amount5.Location = new System.Drawing.Point(434, 26);
+            this.amount5.Name = "amount5";
+            this.amount5.Size = new System.Drawing.Size(16, 14);
+            this.amount5.TabIndex = 5;
+            this.amount5.Text = "10";
+            // 
+            // amount4
+            // 
+            this.amount4.AutoSize = true;
+            this.amount4.ForeColor = System.Drawing.Color.Red;
+            this.amount4.Location = new System.Drawing.Point(330, 25);
+            this.amount4.Name = "amount4";
+            this.amount4.Size = new System.Drawing.Size(16, 14);
+            this.amount4.TabIndex = 4;
+            this.amount4.Text = "10";
+            // 
+            // amount3
+            // 
+            this.amount3.AutoSize = true;
+            this.amount3.ForeColor = System.Drawing.Color.Red;
+            this.amount3.Location = new System.Drawing.Point(226, 25);
+            this.amount3.Name = "amount3";
+            this.amount3.Size = new System.Drawing.Size(16, 14);
+            this.amount3.TabIndex = 3;
+            this.amount3.Text = "10";
+            // 
+            // amount2
+            // 
+            this.amount2.AutoSize = true;
+            this.amount2.ForeColor = System.Drawing.Color.Red;
+            this.amount2.Location = new System.Drawing.Point(117, 25);
+            this.amount2.Name = "amount2";
+            this.amount2.Size = new System.Drawing.Size(16, 14);
+            this.amount2.TabIndex = 2;
+            this.amount2.Text = "10";
+            // 
+            // amount1
+            // 
+            this.amount1.AutoSize = true;
+            this.amount1.ForeColor = System.Drawing.Color.Red;
+            this.amount1.Location = new System.Drawing.Point(20, 25);
+            this.amount1.Name = "amount1";
+            this.amount1.Size = new System.Drawing.Size(16, 14);
+            this.amount1.TabIndex = 1;
+            this.amount1.Text = "10";
             // 
             // pictureBox10
             // 
@@ -586,105 +707,75 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "액션 :";
             // 
-            // amount1
+            // label4
             // 
-            this.amount1.AutoSize = true;
-            this.amount1.ForeColor = System.Drawing.Color.Red;
-            this.amount1.Location = new System.Drawing.Point(20, 25);
-            this.amount1.Name = "amount1";
-            this.amount1.Size = new System.Drawing.Size(16, 14);
-            this.amount1.TabIndex = 1;
-            this.amount1.Text = "10";
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(13, 38);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(16, 14);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "10";
             // 
-            // amount2
+            // label5
             // 
-            this.amount2.AutoSize = true;
-            this.amount2.ForeColor = System.Drawing.Color.Red;
-            this.amount2.Location = new System.Drawing.Point(117, 25);
-            this.amount2.Name = "amount2";
-            this.amount2.Size = new System.Drawing.Size(16, 14);
-            this.amount2.TabIndex = 2;
-            this.amount2.Text = "10";
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(65, 38);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(16, 14);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "10";
             // 
-            // amount3
+            // label6
             // 
-            this.amount3.AutoSize = true;
-            this.amount3.ForeColor = System.Drawing.Color.Red;
-            this.amount3.Location = new System.Drawing.Point(226, 25);
-            this.amount3.Name = "amount3";
-            this.amount3.Size = new System.Drawing.Size(16, 14);
-            this.amount3.TabIndex = 3;
-            this.amount3.Text = "10";
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(119, 38);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(16, 14);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "10";
             // 
-            // amount4
+            // label7
             // 
-            this.amount4.AutoSize = true;
-            this.amount4.ForeColor = System.Drawing.Color.Red;
-            this.amount4.Location = new System.Drawing.Point(330, 25);
-            this.amount4.Name = "amount4";
-            this.amount4.Size = new System.Drawing.Size(16, 14);
-            this.amount4.TabIndex = 4;
-            this.amount4.Text = "10";
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.Color.Red;
+            this.label7.Location = new System.Drawing.Point(12, 139);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(16, 14);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "10";
             // 
-            // amount5
+            // label8
             // 
-            this.amount5.AutoSize = true;
-            this.amount5.ForeColor = System.Drawing.Color.Red;
-            this.amount5.Location = new System.Drawing.Point(434, 26);
-            this.amount5.Name = "amount5";
-            this.amount5.Size = new System.Drawing.Size(16, 14);
-            this.amount5.TabIndex = 5;
-            this.amount5.Text = "10";
+            this.label8.AutoSize = true;
+            this.label8.ForeColor = System.Drawing.Color.Red;
+            this.label8.Location = new System.Drawing.Point(65, 139);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(16, 14);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "10";
             // 
-            // amount6
+            // label10
             // 
-            this.amount6.AutoSize = true;
-            this.amount6.ForeColor = System.Drawing.Color.Red;
-            this.amount6.Location = new System.Drawing.Point(20, 155);
-            this.amount6.Name = "amount6";
-            this.amount6.Size = new System.Drawing.Size(16, 14);
-            this.amount6.TabIndex = 6;
-            this.amount6.Text = "10";
+            this.label10.AutoSize = true;
+            this.label10.ForeColor = System.Drawing.Color.Red;
+            this.label10.Location = new System.Drawing.Point(119, 139);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(16, 14);
+            this.label10.TabIndex = 16;
+            this.label10.Text = "10";
             // 
-            // amount7
+            // label11
             // 
-            this.amount7.AutoSize = true;
-            this.amount7.ForeColor = System.Drawing.Color.Red;
-            this.amount7.Location = new System.Drawing.Point(117, 155);
-            this.amount7.Name = "amount7";
-            this.amount7.Size = new System.Drawing.Size(16, 14);
-            this.amount7.TabIndex = 7;
-            this.amount7.Text = "10";
-            // 
-            // amount8
-            // 
-            this.amount8.AutoSize = true;
-            this.amount8.ForeColor = System.Drawing.Color.Red;
-            this.amount8.Location = new System.Drawing.Point(226, 155);
-            this.amount8.Name = "amount8";
-            this.amount8.Size = new System.Drawing.Size(16, 14);
-            this.amount8.TabIndex = 8;
-            this.amount8.Text = "10";
-            // 
-            // amount9
-            // 
-            this.amount9.AutoSize = true;
-            this.amount9.ForeColor = System.Drawing.Color.Red;
-            this.amount9.Location = new System.Drawing.Point(330, 155);
-            this.amount9.Name = "amount9";
-            this.amount9.Size = new System.Drawing.Size(16, 14);
-            this.amount9.TabIndex = 9;
-            this.amount9.Text = "10";
-            // 
-            // amount10
-            // 
-            this.amount10.AutoSize = true;
-            this.amount10.ForeColor = System.Drawing.Color.Red;
-            this.amount10.Location = new System.Drawing.Point(434, 155);
-            this.amount10.Name = "amount10";
-            this.amount10.Size = new System.Drawing.Size(16, 14);
-            this.amount10.TabIndex = 10;
-            this.amount10.Text = "10";
+            this.label11.AutoSize = true;
+            this.label11.ForeColor = System.Drawing.Color.Red;
+            this.label11.Location = new System.Drawing.Point(13, 232);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(16, 14);
+            this.label11.TabIndex = 17;
+            this.label11.Text = "10";
             // 
             // Game_Screen
             // 
@@ -714,6 +805,7 @@
             this.groupBox5.PerformLayout();
             this.groupBox6.ResumeLayout(false);
             this.groupBox7.ResumeLayout(false);
+            this.groupBox7.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox17)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox15)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox16)).EndInit();
@@ -808,6 +900,13 @@
         private System.Windows.Forms.Label amount3;
         private System.Windows.Forms.Label amount2;
         private System.Windows.Forms.Label amount1;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
     }
 }
 
