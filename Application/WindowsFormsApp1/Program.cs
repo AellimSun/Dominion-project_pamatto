8977522b25e3bfd4808dca4c63f4b16f3b167bb7
nya <elim9010@gmail.com>
2022-05-25 13:06:46 +0900

    Application - DTL.dll 수정, app에서 transhandelr 코드 수정

--- Application/WindowsFormsApp1/Program.cs
+++ Application/WindowsFormsApp1/Program.cs
@@ -12,13 +12,11 @@ namespace WindowsFormsApp1
     public static class Global
     {
         public static string UserID;
-        public static TransHandler transHandler = new TransHandler("127.0.0.1", 5524);
+        public static TransHandler transHandler;
     }
 
     internal static class Program
     {
-        public static string UserID = "";
-        public static TransHandler transHandler = new TransHandler("127.0.0.1", 5524);
         //
         /// <summary>
         /// 해당 애플리케이션의 주 진입점입니다.
@@ -26,7 +24,6 @@ namespace WindowsFormsApp1
         [STAThread]
         static void Main()
         {
-           
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form2());
