516deac1f8c5bc18d4d2e9c64f4c5f8e0b696d1d
limlr@naver.com <limlr@naver.com>
2022-05-26 15:42:01 +0900

    33

--- Application/WindowsFormsApp1/Deck.cs
+++ Application/WindowsFormsApp1/Deck.cs
@@ -143,6 +143,8 @@ namespace WindowsFormsApp1
 
         public void DrawToHand(int i, Game_Screen g)
         {
+            game_Screen.MakeString(i);
+
             while (0 < i)
             {
                 HandDeck.Add(DrawDeck[0]);
@@ -152,6 +154,7 @@ namespace WindowsFormsApp1
                 i--;
             }
             g.setHandDeckImg(this);
+
         }
         public void DrawToHand(int i, List<Card> actionlist)        //지워야됨
         {
