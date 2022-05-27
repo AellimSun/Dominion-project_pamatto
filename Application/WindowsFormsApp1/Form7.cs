811e1e94040a9c9c2c8542cba43d20890f88eedb
경길 임 <lkko3579@naver.com>
2022-05-27 15:41:30 +0900

    DTL 추가가쓰

--- Application/WindowsFormsApp1/Form7.cs
+++ Application/WindowsFormsApp1/Form7.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Windows.Forms.Charting;
+//using System.Windows.Forms.Charting;
 
 namespace WindowsFormsApp1
 {
