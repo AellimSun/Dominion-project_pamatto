a014fc90a4affd61a46bd2522dec28e88b8d6cea
경래 최 <rudfo3264@naver.com>
2022-05-27 16:23:09 +0900

    Button1 텍스트, 폰트 변경

--- Application/WindowsFormsApp1/Game_Screen.cs
+++ Application/WindowsFormsApp1/Game_Screen.cs
@@ -64,6 +64,7 @@ namespace WindowsFormsApp1
             groupBox9.Text = "Deck";
             groupBox10.Font = font;
             groupBox10.Text = "My Action / Buy";
+            button1.Font = font;
 
             upper = new PictureBox[] { pictureBox27, pictureBox26, pictureBox25, pictureBox30,
                 pictureBox29, pictureBox28, pictureBox41, pictureBox42, pictureBox43, pictureBox44,
@@ -115,7 +116,7 @@ namespace WindowsFormsApp1
             CSamount7.Text = estateList[3].amount.ToString();
 
             pictureBoxTF();
-            button1.Text = "액션 종료";
+            button1.Text = "Action End";
 
             
         }
@@ -208,7 +209,7 @@ namespace WindowsFormsApp1
                 }
                 else if (clickMode.Equals("grave"))
                 {
-                    MessageBox.Show("핸드에서 카드를 골라 버려야 합니다.\n원하지 않을 경우 효과 종료를 클릭해 주세요.");
+                    MessageBox.Show("핸드에서 카드를 골라 버려야 합니다.\n원하지 않을 경우 Effect End 종료를 클릭해 주세요.");
                     return;
                 }
                 else if (clickMode.Equals("actionEffectMode"))
@@ -218,12 +219,12 @@ namespace WindowsFormsApp1
                 }
                 else if (clickMode.Equals("trash"))
                 {
-                    MessageBox.Show("핸드에서 카드를 폐기해야 합니다.\n원하지 않을 경우 폐기 종료를 클릭해 주세요.");
+                    MessageBox.Show("핸드에서 카드를 폐기해야 합니다.\n원하지 않을 경우 Scrap End를 클릭해 주세요.");
                     return;
                 }
                 else if (clickMode.Equals("moneyTrash"))
                 {
-                    MessageBox.Show("핸드에서 재물 카드를 폐기해야 합니다.\n원하지 않을 경우 폐기 종료를 클릭해 주세요.");
+                    MessageBox.Show("핸드에서 재물 카드를 폐기해야 합니다.\n원하지 않을 경우 Scrap End를 클릭해 주세요.");
                     return;
                 }
                 MakeString(name, "m");
@@ -309,11 +310,11 @@ namespace WindowsFormsApp1
             string state = button1.Text;
             if (state.Equals("Action End"))
             {
-                button1.Text = "구매 종료";
+                button1.Text = "Buy End";
                 game.gameTable.ActionNumber = 0;
                 changeABC(game.gameTable);
             }
-            else if (state.Equals("버리기 종료"))
+            else if (state.Equals("Throw End"))
             {
                 if (selected.Count != 0)
                 {
@@ -330,7 +331,7 @@ namespace WindowsFormsApp1
                     turn_button1("Action End");
                 }
             }
-            else if (state.Equals("효과 종료") || state.Equals("폐기 End"))
+            else if (state.Equals("Effect End") || state.Equals("Scarp End"))
             {
                 clickMode = "market";
                 game.gameTable.Coin = 0;
@@ -338,20 +339,20 @@ namespace WindowsFormsApp1
 
                 if (game.gameTable.ActionNumber == 0)
                 {
-                    turn_button1("구매 종료");
+                    turn_button1("Buy End");
                 }
                 else
                 {
-                    turn_button1("액션 종료");
+                    turn_button1("Action End");
                 }
             }
-            else if (state.Equals("구매 종료"))
+            else if (state.Equals("Buy End"))
             {
                 //턴 종료
                 if (!market.Game_Over())
                 {
                     //Global.transHandler.Turn_end();
-                    button1.Text = "액션 종료";
+                    button1.Text = "Action End";
                     //버튼 비활성화
                     button1.Enabled = false;
                     Listen_Method();
@@ -488,7 +489,7 @@ namespace WindowsFormsApp1
                 }
                 else if (clickMode.Equals("actionEffectMode"))
                 {
-                    MessageBox.Show("구매하실 카드를 클릭해 주세요.\n원하지 않을 경우 효과 종료를 클릭해주세요.");
+                    MessageBox.Show("구매하실 카드를 클릭해 주세요.\n원하지 않을 경우 Effect End를 클릭해주세요.");
                 }
                 else if (clickMode.Equals("trash"))
                 {
@@ -499,7 +500,7 @@ namespace WindowsFormsApp1
 
                     setHandDeckImg(deck);
                     clickMode = "actionEffectMode";
-                    button1.Text = "효과 종료";
+                    button1.Text = "Effect End";
                 }
                 else if (clickMode.Equals("moneyTrash"))
                 {
@@ -524,11 +525,11 @@ namespace WindowsFormsApp1
                     clickMode = "market";
                     if (game.gameTable.ActionNumber == 0)
                     {
-                        turn_button1("구매 종료");
+                        turn_button1("Buy End");
                     }
                     else
                     {
-                        turn_button1("액션 종료");
+                        turn_button1("Action End");
                     }
                 }
             }
