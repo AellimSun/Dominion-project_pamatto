cc6e4233fcafd926274b0d1b7d127cc53939322d
경길 임 <lkko3579@naver.com>
2022-05-27 21:32:56 +0900

    게임종료 폼 거의 마무리

--- Application/WindowsFormsApp1/Form7.Designer.cs
+++ Application/WindowsFormsApp1/Form7.Designer.cs
@@ -43,14 +43,17 @@ namespace WindowsFormsApp1
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend5 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.label50 = new System.Windows.Forms.Label();
             this.listBox50 = new System.Windows.Forms.ListBox();
             this.button50 = new System.Windows.Forms.Button();
             this.pictureBox50 = new System.Windows.Forms.PictureBox();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox50)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
@@ -58,33 +61,34 @@ namespace WindowsFormsApp1
             // label50
             // 
             this.label50.AutoSize = true;
-            this.label50.Font = new System.Drawing.Font("굴림", 15F);
-            this.label50.Location = new System.Drawing.Point(56, 46);
+            this.label50.Font = new System.Drawing.Font("굴림", 20F);
+            this.label50.Location = new System.Drawing.Point(36, 45);
             this.label50.Name = "label50";
-            this.label50.Size = new System.Drawing.Size(96, 20);
+            this.label50.Size = new System.Drawing.Size(129, 27);
             this.label50.TabIndex = 1;
             this.label50.Text = "게임 종료";
             // 
             // listBox50
             // 
+            this.listBox50.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.listBox50.FormattingEnabled = true;
             this.listBox50.ItemHeight = 12;
             this.listBox50.Items.AddRange(new object[] {
             " "});
             this.listBox50.Location = new System.Drawing.Point(15, 95);
             this.listBox50.Name = "listBox50";
-            this.listBox50.Size = new System.Drawing.Size(175, 136);
+            this.listBox50.Size = new System.Drawing.Size(175, 192);
             this.listBox50.TabIndex = 2;
             // 
             // button50
             // 
-            this.button50.Location = new System.Drawing.Point(60, 240);
+            this.button50.Location = new System.Drawing.Point(104, 304);
             this.button50.Name = "button50";
             this.button50.Size = new System.Drawing.Size(75, 23);
             this.button50.TabIndex = 3;
             this.button50.Text = "돌아가기";
             this.button50.UseVisualStyleBackColor = true;
-            this.button50.Click += new System.EventHandler(this.button1_Click);
+            this.button50.Click += new System.EventHandler(this.button50_Click);
             // 
             // pictureBox50
             // 
@@ -98,33 +102,66 @@ namespace WindowsFormsApp1
             // 
             // chart1
             // 
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(43, 95);
+            chartArea5.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea5);
+            legend5.Name = "Legend1";
+            this.chart1.Legends.Add(legend5);
+            this.chart1.Location = new System.Drawing.Point(15, 177);
             this.chart1.Name = "chart1";
-            series1.ChartArea = "ChartArea1";
-            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Bar;
-            series1.Font = new System.Drawing.Font("Bodoni MT Poster Compressed", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            series1.IsXValueIndexed = true;
-            series1.Legend = "Legend1";
-            series1.Name = "Points";
-            this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(147, 113);
+            series5.ChartArea = "ChartArea1";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Bar;
+            series5.Font = new System.Drawing.Font("Bodoni MT Poster Compressed", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            series5.Legend = "Legend1";
+            series5.Name = "Score";
+            this.chart1.Series.Add(series5);
+            this.chart1.Size = new System.Drawing.Size(175, 114);
             this.chart1.TabIndex = 4;
             this.chart1.Text = "chart1";
+            this.chart1.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(23, 304);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "그래프";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(15, 131);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(175, 21);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Location = new System.Drawing.Point(15, 150);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(175, 14);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // Form7
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(468, 351);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.chart1);
             this.Controls.Add(this.button50);
             this.Controls.Add(this.listBox50);
             this.Controls.Add(this.label50);
             this.Controls.Add(this.pictureBox50);
+            this.Location = new System.Drawing.Point(100, 100);
             this.Name = "Form7";
             this.Text = "Form7";
             this.Load += new System.EventHandler(this.Form7_Load);
@@ -142,5 +179,8 @@ namespace WindowsFormsApp1
         private System.Windows.Forms.ListBox listBox50;
         private System.Windows.Forms.Button button50;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private Button button1;
+        private TextBox textBox1;
+        private TextBox textBox2;
     }
 }
